9e778f53122eede95039e7f7ed9426f17d878b1f
ken-wong <wangkun025@gmail.com>
2015-03-02 19:53:54 +0800

    Fix user password issue

--- db/migrate/20150211151211_devise_create_users.rb
+++ db/migrate/20150211151211_devise_create_users.rb
@@ -2,7 +2,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
   def migrate(direction)
     super
     # Create a default user
-    User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if direction == :up
+    User.create!(email: 'admin@example.com', password: 'password123', password_confirmation: 'password123') if direction == :up
   end
 
   def change
