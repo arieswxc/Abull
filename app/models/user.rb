4543ac577b1a17c8f71b4046fc0ffdc02f4a2436
ken-wong <wangkun025@gmail.com>
2015-03-02 15:10:42 +0800

    Add sign_up for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, 
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :password, format: { with: /(?=.*[a-z])(?=.*\d)/i}
   mount_uploader :avatar, AvatarUploader
