31a3f901a2d6251ab2891dfe48f19f7b36e50117
zxnicv <xianjieo@163.com>
2015-03-10 17:00:49 +0800

    write admin/fund.rb

--- app/admin/fund.rb
+++ app/admin/fund.rb
@@ -7,22 +7,42 @@ ActiveAdmin.register Fund do
 
   permit_params :name, :user_id, :amount, :collection_deadline, :earning, :expected_earning_rate, :earning_rate, :state, :private_check, :minimum, :invest_starting_date, :invest_ending_date, :description, :risk_method, :initial_amount, :created_at, :updated_at
   # permit_params :state
-  
-  # filter :state, as: :select, :collection => ["还未审核", "待审核", "已审核", "审核失败"]
-  scope 
+    
   index do
     selectable_column
     column "标名*", :name
     column "募集数额*", :amount
-    column "募集截止时间*", :collection_deadline
     column "最小值*", :minimum
     column "明(暗)标*", :private_check
-    column "操盘开始时间*", :invest_starting_date
-    column "操盘结束时间*", :invest_ending_date
     column "投资方向", :description
     column "风控措施", :risk_method
     column "状态", :state
-    actions
+    column "募集截止时间*", :collection_deadline
+    column "操盘开始时间*", :invest_starting_date
+    column "操盘结束时间*", :invest_ending_date
+    actions defaults: false do |fund|
+      item "Preview", admin_fund_path(fund), class: "member_link"
+      item "Edit", edit_admin_fund_path(fund), class: "member_link"
+    end
+  end
+
+  #   actions
+  # end
+
+  # page of show
+  show do |fund|
+    attributes_table  do 
+      row('标名')           { |f| f.name }
+      row('募集资金')       { |f| f.amount }
+      row('募集截止时间')   { |f| f.collection_deadline }
+      row('最小值')        { |f| f.minimum }
+      row('明(暗)标')      { |f| f.private_check }
+      row('操盘开始时间')   { |f| f.invest_starting_date }
+      row('操盘结束时间')   { |f| f.invest_ending_date }      
+      row('投资方向')      { |f| f.description}
+      row('风控措施')      { |f| f.risk_method }
+      row('状态')          { |f| f.state }      
+    end
   end
 
   # page of new and edit
@@ -35,7 +55,7 @@ ActiveAdmin.register Fund do
         # f.input :invest_starting_date
         # f.input :invest_ending_date
         # f.input :state, as: :select, collection: ["还未审核", "待审核", "已审核", "审核失败"], :label => "状态"
-        f.input :state
+        f.input :state, as: :select, collection: ["pending", "applied", "gathering", "reached", "opened", "running", "finished", "closed"]
         # f.input :reject_reason, :label => "退回说明"
       end
       f.actions
