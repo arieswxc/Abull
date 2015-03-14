b210c53a6f2851a4550c9224e2ab75787520cf92
黄铮铮 <hzz.gofur@gmail.com>
2015-03-14 15:43:58 +0800

    小问题

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,15 +1,3 @@
 $(function() {
-	$(".nav.navbar-nav > li > a").hover(function(){	
-		$(this).children().each(function() {
-			if ($(this).hasClass("social-content")) {
-				$(this).toggle(true);
-			}
-		})
-	},function() {
-		$(this).children().each(function() {
-			if ($(this).hasClass("social-content")) {
-				$(this).toggle(false);
-			}
-		})
-	});
+	
 });
