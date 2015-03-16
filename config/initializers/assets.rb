9001341333c32544fc5540c106ca08c9c920de86
ken-wong <wangkun025@gmail.com>
2015-03-16 09:45:50 +0800

    More compile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( *.js )
 Rails.application.config.assets.precompile += %w( *.css )
+Rails.application.config.assets.precompile += %w( active_admin.css active_admin/print.css active_admin.js )
