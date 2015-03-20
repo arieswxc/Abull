6a464f3a7fc39a456a28fb4267c1ae4ef6421c74
ken-wong <wangkun025@gmail.com>
2015-03-20 22:12:47 +0800

    Fix seed isue

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -2,7 +2,10 @@ class AdminUser < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, 
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,:authentication_keys => [:cell]
   validates :role, presence: true
   validates :role, inclusion: { in: ["risk_controller", "teller", "account_manager", "customer_service"] }
+  def email_required?
+    false
+  end
 end
