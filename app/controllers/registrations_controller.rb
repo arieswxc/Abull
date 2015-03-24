3d938ca16e51726008816b3e666fdcce71951412
zxnicv <xianjieo@163.com>
2015-03-24 17:08:42 +0800

    添加注册短信验证的后台逻辑

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,33 @@
 class RegistrationsController < Devise::RegistrationsController
   after_filter :add_account
   after_filter :add_three_photos
+
+  def create
+    if session[:code] == params[:verification_code]
+      build_resource(sign_up_params)
+  
+      resource.save
+      yield resource if block_given?
+      if resource.persisted?
+        if resource.active_for_authentication?
+          set_flash_message :notice, :signed_up if is_flashing_format?
+          sign_up(resource_name, resource)
+          respond_with resource, location: after_sign_up_path_for(resource)
+        else
+          set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+          expire_data_after_sign_in!
+          respond_with resource, location: after_inactive_sign_up_path_for(resource)
+        end
+      else
+        clean_up_passwords resource
+        set_minimum_password_length
+        respond_with resource
+      end
+    else
+      redirect_to new_registration_path(resource_name)
+    end
+  end
+
   protected
     def update_resource(resource, params)
       resource.update_without_password(params)
