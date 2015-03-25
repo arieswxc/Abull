6c3cdb58b0722db3aabb23d23bbeb930c48643ab
ken-wong <wangkun025@gmail.com>
2015-03-25 18:40:41 +0800

    Fix migration issue

--- db/migrate/20150325093539_change_account.rb
+++ db/migrate/20150325093539_change_account.rb
@@ -1,5 +1,5 @@
 class ChangeAccount < ActiveRecord::Migration
   def change
-    add_column :accounts, :frost , precision: 12, scale: 2, default: 0
+    add_column :accounts, :frost, :decimal , precision: 12, scale: 2, default: 0
   end
 end
