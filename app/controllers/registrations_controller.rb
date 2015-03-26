b4aff88d0e753c25cf0649d0a9e53e22f6eaac20
zxnicv <xianjieo@163.com>
2015-03-26 17:44:16 +0800

    fix bugs

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,12 +8,11 @@ class RegistrationsController < Devise::RegistrationsController
     if session[:code] != params[:verification_code].to_i
       flash[:notice] = "验证码错误"
       redirect_to new_registration_path(resource_name)
-    elsif User.find_by(user_name: params[:user_name])
+    elsif User.find_by(username: params[:user][:username])
       flash[:notice] = "用户昵称已经被注册，请更换"
       redirect_to new_registration_path(resource_name)
     else
       build_resource(sign_up_params)
-  
       resource.save
       yield resource if block_given?
       if resource.persisted?
@@ -31,8 +30,36 @@ class RegistrationsController < Devise::RegistrationsController
         set_minimum_password_length
         respond_with resource
       end
-    end      
+    end
   end
+  #   if session[:code] == params[:verification_code].to_i && User.find_by(username: params[:username]).nil?
+  #     build_resource(sign_up_params)
+  
+  #     resource.save
+  #     yield resource if block_given?
+  #     if resource.persisted?
+  #       if resource.active_for_authentication?
+  #         set_flash_message :notice, :signed_up if is_flashing_format?
+  #         sign_up(resource_name, resource)
+  #         respond_with resource, location: after_sign_up_path_for(resource)
+  #       else
+  #         set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+  #         expire_data_after_sign_in!
+  #         respond_with resource, location: after_inactive_sign_up_path_for(resource)
+  #       end
+  #     else
+  #       clean_up_passwords resource
+  #       set_minimum_password_length
+  #       respond_with resource
+  #     end
+  #   elsif session[:code] != params[:verification_code].to_i
+  #     flash[:notice] = "验证码错误"
+  #     redirect_to new_registration_path(resource_name)
+  #   else
+  #     flash[:notice] = "用户昵称已经被注册，请更换"
+  #     redirect_to new_registration_path(resource_name)
+  #   end
+  # end
 
   protected
     def update_resource(resource, params)
@@ -40,13 +67,13 @@ class RegistrationsController < Devise::RegistrationsController
     end
 
     def add_account
-      if resource.account.nil?
+      if resource && resource.account.nil?
         resource.create_account
       end
     end
 
     def add_three_photos
-      if resource.persisted?
+      if resource && resource.persisted?
         resource.photos.create(title: "身份证正面")
         resource.photos.create(title: "身份证反面")
         resource.photos.create(title: "手持身份证露脸照片")
