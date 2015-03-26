256fef33ff4b4e53b3805499bb991e0d1dc9fc7c
黄铮铮 <hzz.gofur@gmail.com>
2015-03-26 15:14:03 +0800

    错误显示

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,38 @@
 //= require_tree .
 //= require jquery_nested_form
 //= require 'china_city/jquery.china_city'
+
+
+function check_length(em){
+  em.each(function(){
+    var _self = this;
+    var tmpval = $(_self).val().length,
+     maxl = parseInt($(_self).attr("maxlength")),
+     minl = parseInt($(_self).attr("minlength"));
+    if (tmpval > maxl || tmpval < minl ) {
+      $(_self.parentNode).addClass("state-error");
+    } else {
+      $(_self.parentNode).removeClass("state-error");
+    }
+  });
+}
+function check_val(em){
+  em.each(function(){
+    var _self = this;
+    var tmpval = parseInt($(_self).val()) || 0;
+    if (tmpval > 0) {
+      $(_self.parentNode).removeClass("state-error");
+    } else {
+      $(_self.parentNode).addClass("state-error");
+    }
+  });
+}
+function scrollToErr() {
+  if ($(".state-error").length > 0) {
+    var scroll_offset = $(".state-error").offset(); //得到pos这个div层的offset，包含两个值，top和left
+    $("body,html").animate({
+      scrollTop:scroll_offset.top //让body的scrollTop等于pos的top，就实现了滚动
+    },300);
+    return;
+  }
+}
