439ab822ee92688afc9d82f142d205f249b2f55e
ken-wong <wangkun025@gmail.com>
2015-03-29 16:13:08 +0800

    Add state to billings

--- spec/factories/billings.rb
+++ spec/factories/billings.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
     association   :account
     amount        "9.99"
     billing_type  "MyString"
+    state         "pending"
     association   :billable, factory: :invest
   end
 end
