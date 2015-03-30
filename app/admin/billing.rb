1aa6da5e1574e73551ecbbfe774ae362c2ea4606
ken-wong <wangkun025@gmail.com>
2015-03-30 13:36:29 +0800

    Control the state of billing in backend

--- app/admin/billing.rb
+++ app/admin/billing.rb
@@ -1,3 +1,39 @@
 ActiveAdmin.register Billing do
-  permit_params :account_id, :amount, :billing_type, :billable_type, :billable_id
+  permit_params :account_id, :amount, :billing_type, :billable_type, :billable_id, :state
+
+  show do |billing|
+    attributes_table do
+      row :id
+      row :account_id
+      row :amount
+      row :billing_type
+      row :billable_type
+      row :billable_id
+      row :state
+    end
+  end
+
+  sidebar "State", only: :show do
+    attributes_table_for billing do
+      row :state
+      row('Confirm')  do 
+          link_to t('Confirm'), confirm_admin_billing_path(billing), :method => :put, :class => 'button' 
+      end
+      row('Deny')  do 
+          link_to t('Deny'), deny_admin_billing_path(billing), :method => :put, :class => 'button' 
+      end
+    end
+  end
+
+  member_action :confirm, :method => :put do
+    billing = Billing.find(params[:id])
+    billing.confirm
+    redirect_to admin_billing_path(billing)
+  end
+
+  member_action :deny, :method => :put do
+    billing = Billing.find(params[:id])
+    billing.deny
+    redirect_to admin_billing_path(billing)
+  end
 end
