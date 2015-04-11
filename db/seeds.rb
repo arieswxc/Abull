8fbfe049952b3cbfdb15aa801ac3ebe152f7f402
zxnicv <xianjieo@163.com>
2015-04-11 21:47:59 +0800

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,128 +24,63 @@ Account.create!(user_id: admin.id, balance: 0)
 
 User.create!(
   username: 'zjs01',
-  cell: '17601572001',
+  cell: '00000000001',
   password: 'a00000000',
   password_confirmation: 'a00000000',
-  real_name: '张在',
-  id_card_number: '330303030303030303',
-  abstract: '嘎的打击手段大事啊呃我打发打发啊多少发多少访问打发打发打发',
-  level: 'investor',
-  birthday: '1990-01-01',
-  gender: 'male',
-  education: '本科',
-  address: '上海中山',
-  job: '程序员'
+  level: 'investor_applied'
   )
 
 User.create!(
   username: 'zjs02',
-  cell: '17601572002',
+  cell: '00000000002',
   password: 'a00000000',
-  password_confirmation: 'a00000000',
-  real_name: '张在一',
-  id_card_number: '330303030303030303',
-  abstract: '嘎的打击手段大事啊呃我打发打发啊多少发多少访问打发打发打发',
-  level: 'investor',
-  birthday: '1990-01-01',
-  gender: 'male',
-  education: '本科',
-  address: '上海中山',
-  job: '程序员'
+  password_confirmation: 'a00000000'
   )
 
 User.create!(
   username: 'zjs03',
-  cell: '17601572003',
+  cell: '00000000003',
   password: 'a00000000',
   password_confirmation: 'a00000000',
-  real_name: '张在三',
-  id_card_number: '330303030303030303',
-  abstract: '嘎的打击手段大事啊呃我打发打发啊多少发多少访问打发打发打发',
-  level: 'investor',
-  birthday: '1990-01-01',
-  gender: 'male',
-  education: '本科',
-  address: '上海中山',
-  job: '程序员'
+  level: 'investor_applied'
   )
 
 User.create!(
   username: 'zjs04',
-  cell: '17601572004',
+  cell: '00000000004',
   password: 'a00000000',
   password_confirmation: 'a00000000',
-  real_name: '张在四',
-  id_card_number: '330303030303030303',
-  abstract: '嘎的打击手段大事啊呃我打发打发啊多少发多少访问打发打发打发',
-  level: 'investor',
-  birthday: '1990-01-01',
-  gender: 'male',
-  education: '本科',
-  address: '上海中山',
-  job: '程序员'
+  level: 'investor_applied'
   )
 
 User.create!(
   username: 'zjs05',
-  cell: '17601572005',
+  cell: '00000000005',
   password: 'a00000000',
   password_confirmation: 'a00000000',
-  real_name: '张在五',
-  id_card_number: '330303030303030303',
-  abstract: '嘎的打击手段大事啊呃我打发打发啊多少发多少访问打发打发打发',
-  level: 'investor',
-  birthday: '1990-01-01',
-  gender: 'male',
-  education: '本科',
-  address: '上海中山',
-  job: '程序员'
+  level: 'investor_applied'
   )
 
 User.create!(
   username: 'zjs06',
-  cell: '17601572006',
+  cell: '00000000006',
   password: 'a00000000',
   password_confirmation: 'a00000000',
-  real_name: '张在六',
-  id_card_number: '330303030303030303',
-  abstract: '嘎的打击手段大事啊呃我打发打发啊多少发多少访问打发打发打发',
-  level: 'trader',
-  birthday: '1990-01-01',
-  gender: 'male',
-  education: '本科',
-  address: '上海中山',
-  job: '程序员'
+  level: 'investor_applied'
   )
 
 User.create!(
   username: 'zjs07',
-  cell: '17601572007',
+  cell: '00000000007',
   password: 'a00000000',
   password_confirmation: 'a00000000',
-  real_name: '张在七',
-  id_card_number: '330303030303030303',
-  abstract: '嘎的打击手段大事啊呃我打发打发啊多少发多少访问打发打发打发',
-  level: 'trader',
-  birthday: '1990-01-01',
-  gender: 'male',
-  education: '本科',
-  address: '上海中山',
-  job: '程序员'
+  level: 'investor_applied'
   )
 
 User.create!(
   username: 'zjs08',
-  cell: '17601572008',
+  cell: '00000000008',
   password: 'a00000000',
   password_confirmation: 'a00000000',
-  real_name: '张在八',
-  id_card_number: '330303030303030303',
-  abstract: '嘎的打击手段大事啊呃我打发打发啊多少发多少访问打发打发打发',
-  level: 'trader',
-  birthday: '1990-01-01',
-  gender: 'male',
-  education: '本科',
-  address: '上海中山',
-  job: '程序员'
+  level: 'investor_applied'
   )
