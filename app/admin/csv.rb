bff886cc96d46751a426730f06785bc3abaa16bf
ken-wong <wangkun025@gmail.com>
2015-04-14 01:02:41 +0800

    Remove .0 if the number is a integer

--- app/admin/csv.rb
+++ app/admin/csv.rb
@@ -7,7 +7,7 @@ ActiveAdmin.register_page "Csv" do
       # div do
       #   link_to t("file")
       # end
-      para link_to t("上传每日账单数据"), admin_csv_file_path(CsvFile.find_by(title: "每日账单数据"))
+      # para link_to t("上传每日账单数据"), admin_csv_file_path(CsvFile.find_by(title: "每日账单数据"))
       para link_to t('执行'), admin_csv_add_event_path, :method => :get, :class => 'button' 
     end
     panel "导入利率表" do
@@ -17,7 +17,7 @@ ActiveAdmin.register_page "Csv" do
       # div do
       #   link_to t("file")
       # end
-      para link_to t("上传利率表"), admin_csv_file_path(CsvFile.find_by(title: "利率表"))
+      # para link_to t("上传利率表"), admin_csv_file_path(CsvFile.find_by(title: "利率表"))
       para link_to t('执行'), admin_csv_parse_interest_path, :method => :get, :class => 'button' 
     end
   end
